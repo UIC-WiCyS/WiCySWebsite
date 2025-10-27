53071723c02bfbcd7bda391b533d788c3382ee30
hooriyakazmi <hkazmi312@gmail.com>
2025-10-26 22:48:39 -0500

    Added folder svg

--- src/pages/Events Page/Events.jsx
+++ src/pages/Events Page/Events.jsx
@@ -3,7 +3,7 @@ import TitleBar from "../../components/TitleBar.jsx";
 export default function Events() {
     return (
         <main>
-            <TitleBar title="Upcoming Events" />
-        </main>
+            <TitleBar title="Upcoming Events" />       
+        </main>     
     )
 }
