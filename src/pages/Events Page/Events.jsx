9fb01b3832223d075ca865680001c24c3ee57ac5
hooriyakazmi <hkazmi312@gmail.com>
2025-10-26 23:14:35 -0500

    added folder buttons(6)

--- src/pages/Events Page/Events.jsx
+++ src/pages/Events Page/Events.jsx
@@ -1,9 +1,48 @@
 import TitleBar from "../../components/TitleBar.jsx";
+import folderImg from "../../assets/picsEvents/folder.svg";
+
 
 export default function Events() {
     return (
         <main>
-            <TitleBar title="Upcoming Events" />       
+            <TitleBar title="Upcoming Events" />  
+        
+    
+      <div
+        style={{
+          display: "grid",
+          gridTemplateColumns: "repeat(2, 1fr)",
+          gap: "2rem",
+          justifyItems: "center",
+          marginTop: "3rem",
+        }}
+      >
+        {[...Array(6)].map((_, i) => (
+          <button
+            key={i}
+            style={{
+              background: "none",
+              border: "none",
+              cursor: "pointer",
+            }}
+          >
+            <img
+              src={folderImg}
+              alt={`Folder ${i + 1}`}
+              style={{
+                width: "220px",
+                transition: "transform 0.2s",
+              }}
+              onMouseOver={(e) =>
+                (e.currentTarget.style.transform = "scale(1.05)")
+              }
+              onMouseOut={(e) =>
+                (e.currentTarget.style.transform = "scale(1)")
+              }
+            />
+          </button>
+        ))}
+      </div>
         </main>     
     )
 }
