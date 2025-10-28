571b35c7f72ec3f086fc3fd6cd9dbe37ff8b3c1f
acama5 <acama@uic.edu>
2025-10-28 00:22:31 -0500

    added photos into the board page

--- src/pages/Board Page/Board.jsx
+++ src/pages/Board Page/Board.jsx
@@ -6,17 +6,9 @@ import aye from "../../assets/boardpics/aye-treasurer.png"
 import coda from "../../assets/boardpics/coda-prez.png"
 import raima from "../../assets/boardpics/raima-secretary.png"
 import saja from "../../assets/boardpics/saja-engagedirector.png"
-import yuppar from "../../assets/boardpics/yuppar-evp.png"
+import yuppar from "../../assets/boardpics/yuupar-evp.png"
 
 //const IMAGES = [aleena, aye, coda, raima, saja, yuppar]
-
-// function FirstButton(){
-//     return(
-//         <button>First Button</button>
-//     );
-// }
-
-
 
 export default function Board(){ 
     return (
@@ -24,13 +16,12 @@ export default function Board(){
             <TitleBar title="Our Board" />
             <h1>Board Members</h1>
 
-            <img src={aleena} alt="Photo of the IVP Alenna " />
-            <img src={aye} alt="Photo of the treasurer Aye" />
-            <img src={coda} alt="Photo of the president Coda" />
-            <img src={raima} alt="Photo of secretary Raima" />
-            <img src={saja} alt="Photo of engagedirector Saja" />
-            <img src={yuppar} alt="Photo of EVP Yuppar" />
-            
+            <img className= "portraits" src={aleena} alt="Photo of the IVP Alenna " />
+            <img className= "portraits" src={aye} alt="Photo of the treasurer Aye" />
+            <img className= "portraits" src={coda} alt="Photo of the president Coda" />
+            <img className= "portraits" src={raima} alt="Photo of secretary Raima" />
+            <img className= "portraits" src={saja} alt="Photo of engagedirector Saja" />
+            <img className= "portraits" src={yuppar} alt="Photo of EVP Yuppar" />
         </main>
     )
 }
