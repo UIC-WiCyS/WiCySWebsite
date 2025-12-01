4a8c0d872aebaf66836d16115132c457891a4300
acama5 <acama@uic.edu>
2025-11-30 23:03:54 -0600

    Added all of the members

--- src/pages/Board Page/Board.jsx
+++ src/pages/Board Page/Board.jsx
@@ -2,23 +2,91 @@ import TitleBar from "../../components/TitleBar.jsx";
 import './Board.css'
 
 import coda from "../../assets/boardpics/coda-prez.png"
+import yuupar from "../../assets/boardpics/yuupar-evp.png"
+import aleena from "../../assets/boardpics/aleena-ivp.png"
+import saja from "../../assets/boardpics/saja-engagedirector.png"
+import aye from "../../assets/boardpics/aye-treasurer.png"
+import raima from "../../assets/boardpics/raima-secretary.png"
+
 
 export default function Board(){ 
     return (
         <main>
-            <TitleBar title="Our Board"/>
+            <div className = "TitleBar">
+                <TitleBar title="Our Board"/>
+            </div>
 
             <div className= "board">
-            
-                <div className = "member"> 
+                
+                <div className = "memberLeft"> 
                     <div className="image"> 
                         <img src={coda} alt="Photo of the president Coda"/> 
                     </div>
                     <div className="text">
-                        
                         <p className = "name"> Coda Richmond</p>
                         <p className = "position">Executive Position:</p>
                         <p className = "position">President</p>
+                    </div>
+                </div>
+
+                <div className = "memberRight"> 
+                    
+                    <div className="text">
+                        <p className = "name">Yuupar</p>
+                        <p className = "position">Executive Position:</p>
+                        <p className = "position">External Vice President</p>
+                    </div>
+
+                    <div className="image"> 
+                        <img src={yuupar} alt="Photo of the vice president Yuupar"/> 
+                    </div>
+                </div>
+
+                <div className = "memberLeft"> 
+                    <div className="image"> 
+                        <img src={aleena} alt="Photo of the internal vice president Aleena"/> 
+                    </div>
+                    <div className="text">
+                        <p className = "name"> Aleena</p>
+                        <p className = "position">Executive Position:</p>
+                        <p className = "position">Internal Vice President</p>
+                    </div>
+                </div>
+
+                <div className = "memberRight"> 
+                    
+                    <div className="text">
+                        <p className = "name">Saja</p>
+                        <p className = "position">Executive Position:</p>
+                        <p className = "position">Engagement Director</p>
+                    </div>
+
+                    <div className="image"> 
+                        <img src={saja} alt="Photo of the engagement director Saja"/> 
+                    </div>
+                </div>
+
+                <div className = "memberLeft"> 
+                    <div className="image"> 
+                        <img src={aye} alt="Photo of the treasurer Aye"/> 
+                    </div>
+                    <div className="text">
+                        <p className = "name">Aye</p>
+                        <p className = "position">Executive Position:</p>
+                        <p className = "position">Treasurer</p>
+                    </div>
+                </div>
+
+                <div className = "memberRight"> 
+                    
+                    <div className="text">
+                        <p className = "name">Raima</p>
+                        <p className = "position">Executive Position:</p>
+                        <p className = "position">Secratary</p>
+                    </div>
+
+                    <div className="image"> 
+                        <img src={raima} alt="Photo of the secratary Raima"/> 
                     </div>
                 </div>
 
