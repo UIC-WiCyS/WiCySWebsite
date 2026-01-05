2c459edb00e9a42ff41e0a291a6364edd30b1a54
hooriyakazmi <hkazmi312@gmail.com>
2026-01-05 13:36:16 -0600

    added slides section to event cards

--- src/pages/Events Page/Oct.jsx
+++ src/pages/Events Page/Oct.jsx
@@ -28,6 +28,12 @@ export default function Oct() {
           <p><strong>Date & Time:</strong> 10/1/25, 5 PM - 6 PM </p>
           <p><strong>Location:</strong> EIB 124 </p>
           <p><strong>Details:</strong> Job how they secured a cyber job,day in the life of what they do in their job, what tools they recommend to learn based off of what they do and what skills they reccomend you should study if you want to x cyber job. heres how to advance your career </p>
+          <p>
+            <strong>Slides:</strong>
+              <a href="/slides/WiCys-October-2025-Zebra.pdf" target="_blank">
+                 Zebra Slides
+              </a>
+            </p>
           
   
         </div>
@@ -41,6 +47,12 @@ export default function Oct() {
           <p><strong>Date & Time:</strong> 10/6/25, 5 PM - 6 PM  </p>
           <p><strong>Location:</strong> EIB 124</p>
           <p><strong>Details:</strong> Helps students get a grasp of what cybersecurity is and it's importance of risk management with beginner level case studies. </p>
+          <p>
+            <strong>Slides:</strong>
+              <a href="/slides/WiCyS-Network-Security-Lab-2025.pdf" target="_blank">
+                 Network Security Lab
+              </a>
+            </p>
           
   
         </div>
@@ -55,7 +67,8 @@ export default function Oct() {
           <p><strong>Location:</strong> MRH Coldstone </p>
           <p><strong>Details:</strong> From 11am - 9pm Students can go to Coldstone and donate to us buy purchasing Ice cream! They also have a chance to interact with some of the board from 4-6. In order for the donation to be successful,you must tell the cashier that a part of your purchase will go to WiCys! </p>
           
-  
+          
+
         </div>
         <div className="event-card-passed">
             <img
@@ -67,6 +80,12 @@ export default function Oct() {
           <p><strong>Date & Time:</strong> 10/13/25, 5 PM - 6 PM </p>
           <p><strong>Location:</strong> EIB 124 </p>
           <p><strong>Details:</strong> An introduction to John the Ripper. Which is a well known free password cracking software tool! </p>
+          <p>
+            <strong>Slides:</strong>
+              <a href="/slides/John-the-Ripper-Presentation" target="_blank">
+                 John the Ripper Slides
+              </a>
+            </p>
           
   
         </div>
@@ -80,6 +99,12 @@ export default function Oct() {
           <p><strong>Date & Time:</strong> 10/22/25, 5 PM - 6 PM </p>
           <p><strong>Location:</strong> EIB 124 </p>
           <p><strong>Details:</strong> Casual chat about career navigation within cybersecurity, what certifications to get, how IT experience helps, where to apply for interships. Oriented around people who aren't CS majors or people who aren't sure what career path to take.  </p>
+          <p>
+            <strong>Slides:</strong>
+              <a href="/slides/Morningstar.pdf" target="_blank">
+                 Morningstar Guest Speaker Slides
+              </a>
+            </p>
           
   
         </div>
@@ -107,6 +132,12 @@ export default function Oct() {
           <p><strong>Date & Time:</strong> 10/28/25, 5 PM - 6 PM </p>
           <p><strong>Location:</strong> EIB 124 </p>
           <p><strong>Details:</strong> What cyber security is like in the field, a day in the life of Incident Response, and walk through an investigation. </p>
+          <p>
+            <strong>Slides:</strong>
+              <a href="/slides/Sidley.pdf" target="_blank">
+                 Sidley Guest Speaker Slides
+              </a>
+            </p>
           
         </div>
         <div className="event-card-passed">
@@ -119,6 +150,12 @@ export default function Oct() {
           <p><strong>Date & Time:</strong> 10/29/25, 5:30 PM - 7 PM </p>
           <p><strong>Location:</strong> ARC 242 </p>
           <p><strong>Details:</strong> Navigate a simple website with 3-5 different prompts, if you input incorrect answer a jumpscare pops up, otherwise proceed to next prompt. Lets see who wins? Muhaha </p>
+          <p>
+            <strong>Slides:</strong>
+              <a href="/slides/Wicys-Hackoween.pdf" target="_blank">
+                 Hackoween CTF Slides
+              </a>
+            </p>
           
         </div>
 
