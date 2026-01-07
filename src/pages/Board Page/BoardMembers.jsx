3672d5875fa60c8e936f56cc1a2993cb53a65902
acama5 <acama@uic.edu>
2026-01-06 23:11:05 -0600

    Modified the buttons and added the board members

---
+++ src/pages/Board Page/BoardMembers.jsx
@@ -0,0 +1,149 @@
+import TitleBar from "../../components/TitleBar.jsx";
+import './Board.css'
+
+import ammani from "../../assets/boardMembers/ammani.jpg"
+import bre from "../../assets/boardMembers/bre.jpg"
+import deeya from "../../assets/boardMembers/deeya.jpg"
+import giselle from "../../assets/boardMembers/giselle.jpg"
+import julia from "../../assets/boardMembers/julia.jpg"
+import krisha from "../../assets/boardMembers/krisha.jpg"
+import martha from "../../assets/boardMembers/martha.jpg"
+import samira from "../../assets/boardMembers/samira.jpg"
+import simran from "../../assets/boardMembers/simran.jpg"
+import srijani from "../../assets/boardMembers/srijani.jpg"
+
+import {Link} from "react-router-dom"
+
+
+export default function BoardExecutive(){ 
+    return (
+        <main>
+            <div className = "TitleBar">
+                <TitleBar title="Our Board"/>
+            </div>
+
+            <div className="buttons">
+                <div className = "executiveButton">
+                    <Link to = "/board">
+                        <button>Executive Board</button>
+                    </Link>
+                </div>
+
+                <div className = "boardButton">
+                    <Link to = "/board/members">
+                        <button>Board</button>
+                    </Link>
+                </div>
+            </div>
+
+            {/* Board Members */}
+
+            <div className= "board">
+
+                <div className = "rightMember">    
+                    <div className="rightPhoto"> 
+                        <img src={ammani}/> 
+                    </div>
+
+                    <div className="caption">
+                        <p className = "name">Ammani</p>
+                    </div>
+                </div>
+
+                <div className = "leftMember">
+                    <div className="leftPhoto"> 
+                        <img src={bre}/> 
+                    </div>
+
+                    <div className="caption">
+                        <p className = "name">Bre</p>
+                    </div>
+
+                </div>
+
+                <div className = "rightMember">    
+                    <div className="rightPhoto"> 
+                        <img src={deeya}/> 
+                    </div>
+
+                    <div className="caption">
+                        <p className = "name">Deeya</p>
+                    </div>
+                </div>
+
+                <div className = "leftMember"> 
+                    <div className="leftPhoto"> 
+                        <img src={giselle}/> 
+                    </div>
+                    <div className="caption">
+                        <p className = "name">Giselle</p>
+                    </div>
+                </div>
+
+                <div className = "rightMember">    
+                    <div className="rightPhoto"> 
+                        <img src={julia}/> 
+                    </div>
+
+                    <div className="caption">
+                        <p className = "name">Julia</p>
+                    </div>
+                </div>
+
+                <div className = "leftMember"> 
+                    <div className="leftPhoto"> 
+                        <img src={krisha}/> 
+                    </div>
+                    <div className="caption">
+                        <p className = "name">Krisha</p>
+                    </div>
+                </div>
+
+                <div className = "rightMember"> 
+                    <div className="rightPhoto"> 
+                        <img src={martha}/> 
+                    </div>
+
+                    <div className="caption">
+                        <p className = "name">Martha</p>
+                    </div>
+                    
+                </div>
+
+                <div className = "leftMember"> 
+                    <div className="leftPhoto"> 
+                        <img src={samira}/> 
+                    </div>
+
+                    <div className="caption">
+                        <p className = "name">Samira</p>
+                    </div>
+                </div>
+
+                <div className = "rightMember"> 
+                    <div className="rightPhoto"> 
+                        <img src={simran}/> 
+                    </div>   
+
+                    <div className="caption">
+                        <p className = "name">Simran</p>
+                    </div>
+                    
+                </div>
+
+                <div className = "leftMember"> 
+                    <div className="leftPhoto"> 
+                        <img src={srijani}/> 
+                    </div>
+                    <div className="caption">
+                        <p className = "name">Srijani</p>
+                    </div>
+                </div>
+
+
+            </div>
+
+
+        </main>
+    );
+}
---
+++ src/pages/Board Page/ExecutiveBoardMembers.jsx
@@ -0,0 +1,118 @@
+import TitleBar from "../../components/TitleBar.jsx";
+import './Board.css'
+
+import coda from "../../assets/boardpics/coda-prez.png"
+import yuupar from "../../assets/boardpics/yuupar-evp.png"
+import aleena from "../../assets/boardpics/aleena-ivp.png"
+import saja from "../../assets/boardpics/saja-engagedirector.png"
+import aye from "../../assets/boardpics/aye-treasurer.png"
+import raima from "../../assets/boardpics/raima-secretary.png"
+
+import {Link} from "react-router-dom"
+
+export default function Board(){ 
+    
+    return (
+        <main>
+
+            <div className = "TitleBar">
+                <TitleBar title= "Our Board"/>
+            </div>
+
+            {/* Buttons */}
+
+            <div className="buttons">
+                <div className = "executiveButton">
+                    <Link to = "/board">
+                        <button>Executive Board</button>
+                    </Link>
+                </div>
+
+                <div className = "boardButton">
+                    <Link to = "/board/members">
+                        <button>Board</button>
+                    </Link>
+                </div>
+            </div>
+            
+            {/* Executive Board Members */}
+
+            <div className= "board">
+                
+                <div className = "leftMember"> 
+                    <div className="executiveLeftPhoto"> 
+                        <img src={coda} alt="Photo of the president Coda"/> 
+                    </div>
+
+                    <div className="caption">
+                        <p className = "name"> Coda Richmond</p>
+                        <p className = "position">Executive Position:</p>
+                        <p className = "position">President</p>
+                    </div>
+                </div>
+
+                <div className = "rightMember"> 
+                    <div className="executiveRightPhoto"> 
+                        <img src={yuupar} alt="Photo of the vice president Yuupar"/> 
+                    </div>
+                    
+                    <div className="caption">
+                        <p className = "name">Yuupar</p>
+                        <p className = "position">Executive Position:</p>
+                        <p className = "position">External Vice President</p>
+                    </div>
+                </div>
+
+                <div className = "leftMember"> 
+                    <div className="executiveLeftPhoto"> 
+                        <img src={aleena} alt="Photo of the internal vice president Aleena"/> 
+                    </div>
+
+                    <div className="caption">
+                        <p className = "name"> Aleena</p>
+                        <p className = "position">Executive Position:</p>
+                        <p className = "position">Internal Vice President</p>
+                    </div>
+                </div>
+
+                <div className = "rightMember"> 
+                    <div className="executiveRightPhoto"> 
+                        <img src={saja} alt="Photo of the engagement director Saja"/> 
+                    </div>
+
+                    <div className="caption">
+                        <p className = "name">Saja</p>
+                        <p className = "position">Executive Position:</p>
+                        <p className = "position">Engagement Director</p>
+                    </div>
+                </div>
+
+                <div className = "leftMember"> 
+                    <div className="executiveLeftPhoto"> 
+                        <img src={aye} alt="Photo of the treasurer Aye"/> 
+                    </div>
+                    
+                    <div className="caption">
+                        <p className = "name">Aye</p>
+                        <p className = "position">Executive Position:</p>
+                        <p className = "position">Treasurer</p>
+                    </div>
+                </div>
+
+                <div className = "rightMember"> 
+                    <div className="executiveRightPhoto"> 
+                        <img src={raima} alt="Photo of the secratary Raima"/> 
+                    </div>
+
+                    <div className="caption">
+                        <p className = "name">Raima</p>
+                        <p className = "position">Executive Position:</p>
+                        <p className = "position">Secratary</p>
+                    </div>
+                </div>
+
+            </div>
+        </main>
+        
+    );
+}
