0142f5eb39eeff2f9de025a3646d70ff9bd7693d
hooriyakazmi <hkazmi312@gmail.com>
2026-01-31 17:34:44 -0600

    Started on home

--- src/pages/Home Page/Home.jsx
+++ src/pages/Home Page/Home.jsx
@@ -3,12 +3,13 @@ import './Home.css'
 
 import pic1 from "../../assets/pics/MainPage/Photo1.JPG"
 import pic2 from "../../assets/pics/MainPage/cat.jpeg"
-import pic3 from "../../assets/pics/MainPage/Photo2.JPG"
+import screen from "../../assets/pics/MainPage/screen.svg"
+import pic3 from "../../assets/pics/MainPage/Photo3.svg"
+
 
 const IMAGES = [pic1, pic2, pic3]
 
 export default function Home() {
-    const [i, setI] = useState(0)
 
     useEffect(() => {
         const id = setInterval(() => {
@@ -16,27 +17,44 @@ export default function Home() {
         }, 2500)
         return () => clearInterval(id)
     }, [])
-
     return (
-    <div className="SecurityCam">
-        <div className="cam-frame">
-            {IMAGES.map((src, idx) => (
-                <img
-                    key={src}
-                    src={src}
-                    alt={`Security frame ${idx + 1}`}
-                    className={`cam__img ${i === idx ? "is-active" : ""}`}
-                    aria-hidden={i !== idx}
-                />
-            ))}
-
-            <div className="cam-overlay"></div>
-            <div className="cam-hud">
-                <span className="rec">● REC</span>
-                <span className="time">TO BE ADDED</span>
-            </div>
+  
+  <div className="Home">
+    <div className="rectangle">
+      <div className="screen-wrapper">
+        <img 
+          src={screen}
+          alt="Screen frame"
+          className="screen-img"
+        />
+        <div className="overlay-text">
+          <h1>Women in<br/>Cybersecurity</h1>
+          <h2>UIC</h2>
+          <button>learn more</button>
         </div>
+        
+        <section class="mission">
+  <div class="mission-card">
+    <img class="mission-img" src={pic3} alt="Team photo" />
+  </div>
+
+  <div class="mission-text">
+    <h2>Our Mission</h2>
+    <div class="mission-line"></div>
+    <p>
+      We are a group of individuals passionate about making a positive impact
+      through collaboration, innovation and community.
+    </p>
+  </div>
+</section>
+
+      </div>
+      
     </div>
-    )
+  </div>
+)
+
+  
+
     
 }
