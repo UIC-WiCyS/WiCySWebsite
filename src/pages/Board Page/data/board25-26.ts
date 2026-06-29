696efa3fffc05216f157cf8e1b0ffd8a290a65ad
Josephine <jbl.noetic@gmail.com>
2026-06-29 00:01:21 -0500

    adding data files for board members

---
+++ src/pages/Board Page/data/board25-26.ts
@@ -0,0 +1,146 @@
+export const boardInfo25 = {
+    year: "2025 - 2026",
+    "exec": [
+        {
+            name: "Khin Yuupar Myat",
+            role: "External Vice President",
+            linkedin: "https://www.linkedin.com/in/heramyat/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Coda Richmond",
+            role: "President",
+            linkedin: "https://www.linkedin.com/in/coda-richmond/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Aleena Mehmood",
+            role: "Internal Vice President",
+            linkedin: "https://www.linkedin.com/in/aleena-mehmood/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Saja Bushara",
+            role: "Engagement Director",
+            linkedin: "https://www.linkedin.com/in/sajabushara/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Aye Kyawt Zin",
+            role: "Treasurer",
+            linkedin: "https://www.linkedin.com/in/ayekyawtzin/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Raima Chowdhury",
+            role: "Secretary",
+            linkedin: "https://www.linkedin.com/in/raima-chowdhury-uic2027/",
+            degree: "",
+            photo: ""
+        },
+    ],
+    "comms": [
+        {
+            name: "Ammani Khan",
+            role: "Communications Chair",
+            linkedin: "https://www.linkedin.com/in/ammanikhan",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Samira Ali",
+            role: "Communications Committee",
+            linkedin: "https://www.linkedin.com/in/samira-ali-552220378/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Malika Syeda",
+            role: "Communications Committee",
+            linkedin: "https://www.linkedin.com/in/malikarsyeda",
+            degree: "",
+            photo: ""
+        }
+    ],
+    "media": [
+        {
+            name: "Breanna Brown",
+            role: "Social Media Chair",
+            linkedin: "https://www.linkedin.com/in/brebrown795/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Srijani Prekki",
+            role: "Social Media Chair",
+            linkedin: "https://www.linkedin.com/in/srijani-prekki/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Simran Ashraf",
+            role: "Social Media Committee",
+            linkedin: "https://www.linkedin.com/in/simran-ashraf-973836342/",
+            degree: "",
+            photo: ""
+        }
+    ],
+    "fund": [
+        {
+            name: "Deeya Rawat",
+            role: "Fundraising Chair",
+            linkedin: "https://www.linkedin.com/in/deeyarawat/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Giselle Lechuga",
+            role: "Fundraising Committee",
+            linkedin: "https://www.linkedin.com/in/gisellelechuga/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Hiba Mirza",
+            role: "Fundraising Committee",
+            linkedin: "https://www.linkedin.com/in/hibamirza0/",
+            degree: "",
+            photo: ""
+        },
+    ],
+    "web": [
+        {
+            name: "Martha Barraza",
+            role: "Web Admin Chair",
+            linkedin: "https://www.linkedin.com/in/martha-barraza",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Angela Camargo",
+            role: "Web Admin Committee",
+            linkedin: "https://www.linkedin.com/in/angela-camargo-/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Hooriya Kazmi",
+            role: "Web Admin Committee",
+            linkedin: "https://www.linkedin.com/in/hkazmi1/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Valeria Marquez Enriquez",
+            role: "Web Admin Committee",
+            linkedin: "https://www.linkedin.com/in/valeria-marquez-enriquez-b3ba22361/",
+            degree: "",
+            photo: ""
+        },
+    ]
+}
---
+++ src/pages/Board Page/data/board26-27.ts
@@ -0,0 +1,130 @@
+export const boardInfo26 = {
+    year: "2026 - 2027",
+    "exec": [
+        {
+            name: "Breanna Brown",
+            role: "Co-President",
+            linkedin: "https://www.linkedin.com/in/brebrown795/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Deeya Rawat",
+            role: "Co-President",
+            linkedin: "https://www.linkedin.com/in/deeyarawat/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Ammani Khan",
+            role: "External Vice President",
+            linkedin: "https://www.linkedin.com/in/ammanikhan/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Srijani Prekki",
+            role: "Internal Vice President",
+            linkedin: "https://www.linkedin.com/in/srijani-prekki/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Samira Ali",
+            role: "Engagement Director",
+            linkedin: "https://www.linkedin.com/in/samira-ali-552220378/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Asha Thomas",
+            role: "Treasurer",
+            linkedin: "https://www.linkedin.com/in/ashathomas346/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Rose Torres",
+            role: "Secretary",
+            linkedin: "https://www.linkedin.com/in/roselyn-torres/",
+            degree: "",
+            photo: ""
+        },
+    ],
+    "comms": [
+        {
+            name: "Malika Syeda",
+            role: "Communications Chair",
+            linkedin: "https://www.linkedin.com/in/malikarsyeda",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Alina Alina",
+            role: "Communications Committee",
+            linkedin: "",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Zona Noman",
+            role: "Communications Committee",
+            linkedin: "https://www.linkedin.com/in/zona-noman-a05479243/",
+            degree: "",
+            photo: ""
+        }
+    ],
+    "media": [
+        {
+            name: "Simran Ashraf",
+            role: "Social Media Chair",
+            linkedin: "https://www.linkedin.com/in/simran-ashraf-973836342/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Valeria Marquez Enriquez",
+            role: "Social Media Committee",
+            linkedin: "https://www.linkedin.com/in/valeria-marquez-enriquez-b3ba22361/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Kim Ngan Tran",
+            role: "Social Media Committee",
+            linkedin: "https://www.linkedin.com/in/kimtran37/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Naomi Perez Lopez",
+            role: "Social Media Committee",
+            linkedin: "",
+            degree: "",
+            photo: ""
+        }
+    ],
+    "web": [
+        {
+            name: "Hooriya Kazmi",
+            role: "Web Admin Co-Chair",
+            linkedin: "https://www.linkedin.com/in/hkazmi1/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Josephine Lee",
+            role: "Web Admin Co-Chair",
+            linkedin: "https://www.linkedin.com/in/josephine-b-l/",
+            degree: "",
+            photo: ""
+        },
+        {
+            name: "Angela Camargo",
+            role: "Web Admin Committee",
+            linkedin: "https://www.linkedin.com/in/angela-camargo-/",
+            degree: "",
+            photo: ""
+        },
+    ]
+}
