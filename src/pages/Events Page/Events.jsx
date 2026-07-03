ca43fbb6b509e0c00a22359315964ba48c6c8521
Josephine <jbl.noetic@gmail.com>
2026-07-02 20:32:20 -0500

    modified events page

--- src/pages/Events Page/Events.jsx
+++ src/pages/Events Page/Events.jsx
@@ -1,38 +1,36 @@
-import { NavLink } from "react-router-dom";
+import "./Events.css"
+import { eventsData } from "./eventsData";
 import TitleBar from "../../components/TitleBar.jsx";
-import folderImg from "../../assets/picsEvents/folder.svg";
-import "./Events.css";
 
 export default function Events() {
-  const MONTHS = [
-    // { label: "Aug", to: "/events/aug" },
-    { label: "Sep", to: "/events/sep" },
-    { label: "Oct", to: "/events/oct" },
-    { label: "Nov", to: "/events/nov" },
-    { label: "Jan", to: "/events/jan" },
-    { label: "Feb", to: "/events/feb" },
-    { label: "Mar", to: "/events/mar" },
-    { label: "Apr", to: "/events/apr" },
-    { label: "May", to: "/events/may" },
-  ];
+	return (
+		<main>
+			<div className = "TitleBar">
+				<TitleBar title= "Upcoming Events"/>
+			</div>
 
-  const folderLink = ({ isActive }) =>
-    "folder-link" + (isActive ? " active" : "");
+			<div className="events-main">
+				{Array.from(eventsData).map((semester, i) => (
+					<section className="events-section" key={i}>
+						<h1>{semester.name}</h1>
+						<div className="flyers-container">
+							{/* no events */}
+							{(semester.events == [] || semester.events.length == 0) && 
+								<p>No events for now. Come back later!</p>
+							}
 
-  return (
-    <main>
-      <TitleBar title="Upcoming Events" />
-
-      <div className="folder-grid">
-        {MONTHS.map(({ label, to }) => (
-          <NavLink key={label} to={to} className={folderLink}>
-            <div className="folder-wrapper">
-              <img src={folderImg} alt={label} className="folder-img" />
-              <span className="folder-label">{label}</span>
-            </div>
-          </NavLink>
-        ))}
-      </div>
-    </main>
-  );
+							{Array.from(semester.events).map((item)=>
+								<div key={item.img}>
+									{/* <img src={new URL(`../../assets/eventFlyers/${item.img}`, import.meta.url).href} alt={item.alt} />  */}
+									<img className="flyer" src={`eventFlyers/${item.img}`} alt={item.alt} /> 
+								</div>
+									
+							)}
+						</div>
+					</section>
+				))}
+			</div>
+			
+		</main>
+	)
 }
