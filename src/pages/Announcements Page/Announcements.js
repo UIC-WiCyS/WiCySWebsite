f7c67421e31520144d06a6e543a5555e262a5864
Josephine <jbl.noetic@gmail.com>
2026-08-12 11:26:16 -0500

    edited announcement texts style

--- src/pages/Announcements Page/Announcements.js
+++ src/pages/Announcements Page/Announcements.js
@@ -1,17 +1,30 @@
+// colors are currently just alternating based on even/odd index
+// maybe in the future we want to control which one? but not necessary right now -JL
 export const announcements = [
     {
-        date: "08/24/26",
-        title: "Semester Start!",
-        body: `welcome back! 1st day, good luck, have fun! GLHF GLHF GLHF `
+        date: "July 27, 2026",
+        time: "1:23 PM",
+        body: `summer VIBES`
     },
     {
-        date: "08/24/26",
-        title: "Semester Start!",
-        body: `welcome back! 1st day, good luck, have fun! 1st day, good luck, have fun! 1st day, good luck, have fun! 1st day, good luck, have fun!`
+        date: "July 31, 2026",
+        time: "10:03 AM",
+        body: `be there or be SQUARE`
     },
     {
-        date: "08/24/26",
-        title: "Semester Start!",
+        date: "August 11, 2026",
+        time: "11:27 AM",
+        body: `WEBSITE UPDATEDDDDD`
+    },
+    {
+        date: "August 24, 2026",
+        time: "8:00 AM",
         body: `welcome back! 1st day, good luck, have fun!`
     },
+    {
+        date: "August 24, 2026",
+        time: "8:00 AM",
+        body: `welcome back! 1st day, good luck, have fun! welcome back! 1st day, good luck, have fun! welcome back! 1st day, good luck, have fun!`
+    },
+    // add new texts here!
 ];
--- src/pages/Announcements Page/Announcements.jsx
+++ src/pages/Announcements Page/Announcements.jsx
@@ -3,6 +3,7 @@ import "./Announcements.css"
 import { announcements } from './Announcements.js';
 import TitleBar from "../../components/TitleBar.jsx";
 import Marquee from "react-fast-marquee";
+import {CheckCheck, Heart} from "lucide-react";
 
 export default function Announcements(){ 
     return (
@@ -18,18 +19,41 @@ export default function Announcements(){
             <div className="announcements">
 
                 {/* title */}
-                <h3 className='announcements-text'>Stay up to date with us!</h3>
+                <h3 className='announcements-head'>Stay up to date with us!</h3>
 
                 {/* texts */}
-                <div className='announcements'>
-                    {announcements.map((item, id)=>(
-                        <div key={id} className="announcements-card" style={{backgroundColor: id % 2 == 0 ? "#C7DE83":"#E899DC", textAlign: id%2==0 ? "right" : "left", alignSelf: id%2==0 ? "end" : "start", borderRadius: id%2==0 ? "50px 50px 5px 50px" : "50px 50px 50px 5px"}}>
-                            <p>
-                                {item.body}
-                            </p>
-                            <p>
-                                {item.date}
-                            </p>
+                <div className='announcements-cont'>
+                    {announcements.map((item, id, arr)=>(
+                        <div key={id} style={{width: "100%", display: 'flex', flexDirection: 'column'}}>
+                            {
+                                id==0 && 
+                                <p style={{color:"#C2CEAF", fontSize: "small"}}>{item.date}</p>
+                            }
+                            { // check if dates are the same
+                                id!=0 && item.date!=announcements[id-1].date && 
+                                <p style={{color:"#C2CEAF", fontSize: "small"}}>{item.date}</p>
+                            }
+                            <div className="announcements-card" style={{alignItems: id%2==0 ? "end" : "start"}}>
+                                <div style={{display: "flex", alignItems: "center", gap: "1em", justifyContent: id%2==0 ? "end" : "start"}}>
+                                    <p className="announcements-text" style={{textAlign: id%2==0 ? "right" : "left", backgroundColor: id % 2 == 0 ? "#C7DE83":"#E899DC", borderRadius: id%2==0 ? "50px 50px 5px 50px" : "50px 50px 50px 5px"}}>{item.body}</p>
+                                    {/* heart symbol */}
+                                    {
+                                        id%2!=0 && 
+                                        <Heart className="text-heart" color="#0F8B8D" strokeWidth={1} size={32} />
+                                    }  
+                                </div>
+                                
+                                <div style={{display: "flex", justifyContent: "center", alignItems: "center", gap: "1rem"}}>
+                                    {/* time */}
+                                    <p style={{color:"#C2CEAF", fontSize: "small", lineHeight: 0}}>{item.time}</p>
+                                    
+                                    {/* read symbol */}
+                                    {
+                                        id%2==0 && 
+                                        <CheckCheck color="#0F8B8D" strokeWidth={1} size={16}/>
+                                    }
+                                </div>
+                            </div>
                         </div>
                     ))}
                 </div>
